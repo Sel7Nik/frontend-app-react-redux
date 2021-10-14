5963093d0c235661d2dd8eff9a0e4d28026e7336
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-14 22:46:12 +0300

    prepare sort

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import Bank from './components/Bank'
+import React from 'react'
+import style from './style.css'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  asyncDecrementCreator,
+  asyncIncrementCreator,
+  decrementCreator,
+  incrementCreator,
+} from './store/countReducer'
+import { fetchUsers } from './store/userReducer'
 
-function App() {
+const App = () => {
+  const count = useSelector((state) => state.countReducer.count)
+  const users = useSelector((state) => state.userReducer.users)
+  const dispatch = useDispatch()
+
   return (
-    <div>
-      <Bank />
+    <div className="app">
+      <div className="count">{count}</div>
+      <div className="btns">
+        <button
+          className="btn"
+          onClick={() => dispatch(asyncIncrementCreator())}
+        >
+          ИНКРЕМЕНТ++
+        </button>
+        <button
+          className="btn"
+          onClick={() => dispatch(asyncDecrementCreator())}
+        >
+          ДЕКРЕМЕНТ--
+        </button>
+        <button className="btn" onClick={() => dispatch(fetchUsers())}>
+          ПОЛУЧИТЬ ЮЗЕРОВ--
+        </button>
+      </div>
+      <div className="users">
+        {users.map((user) => (
+          <div className="user">{user.name}</div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,17 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import { cashReducer } from './cashReducer'
-import { customerReducer } from './customReducer'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import countReducer from './countReducer'
+import userReducer from './userReducer'
+import createSagaMiddleware from 'redux-saga'
+import { countWatcher } from '../saga/countSaga'
+import { rootWatcher } from '../saga'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const rootReducer = combineReducers({
-  cash: cashReducer,
-  customers: customerReducer,
+  countReducer,
+  userReducer,
 })
-//!--- store
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-)
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(rootWatcher)
